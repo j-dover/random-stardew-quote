510684d73d1a3a3cc6a4fea542420d33aee4fd39
j-dover <aldover27@gmail.com>
2020-03-12 16:27:03 -0400

    implemented server and db connection

---
+++ db.js
@@ -0,0 +1,41 @@
+// Connect to database
+'use strict'
+const sqlite3 = require('sqlite3').verbose();
+let schema = `CREATE TABLE IF NOT EXISTS Villager (
+  villager_id integer PRIMARY KEY AUTOINCREMENT,
+  villager_name text NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS Quotes (
+  quote_id integer PRIMARY KEY AUTOINCREMENT,
+  villager_id integer UNIQUE,
+  quote text NOT NULL UNIQUE,
+  FOREIGN KEY (villager_id) REFERENCES Villager(villager_id)
+);`;
+
+let db = new sqlite3.Database('./random-stardew-quotes.db', (err) => {
+  if (err) {
+    console.error(err.message);
+    return;
+  }
+  else {
+    console.log("Currently connected to the API's SQlite database");
+    db.exec('PRAGMA foreign_keys = ON;', function(err) {
+      if (err) {
+        console.error(err.message);
+      }
+      else {
+        console.log("Database foreign key support is on");
+      }
+    });
+
+    db.exec(schema, (err) => {
+      if (err) {
+        console.log(err.message);
+      }
+    });
+  }
+});
+
+module.exports.sqlite3 = sqlite3;
+module.exports.db = db;
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+'use strict'
 const express = require('express');
 const bodyParser = require('body-parser');
+const dbImport = require(__dirname + '/db.js');
+const sqlite3 = dbImport.sqlite3;
+let db = dbImport.db;
 
 const app = express();
 const options = {
@@ -11,6 +15,60 @@ app.use(express.static("public"));
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.sendFile(__dirname + "/public/index.html"));
+const server = app.listen(port, () => console.log(`Random Stardew Valley Quote API Server is listening on port ${port}`));
 
-app.listen(port, () => console.log(`Random Stardew Valley Quote API Server is listening on port ${port}`));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+  console.log('This is the home page.');
+});
+
+app.get('/api/quotes', (req, res, next) =>{
+  // Get all quotes
+  var sql = 'SELECT * from Quotes';
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      res.status(400).json({"error":err.message});
+      console.error(err.message);
+      return;
+    }
+    res.json({
+      "Quotes":rows
+    });
+  });
+});
+
+app.get("/api/villagers", (req, res, next) => {
+  // Get all villagers
+  var sql = 'SELECT * from Villager';
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      res.status(400).json({"error":err.message});
+      console.error(err.message);
+      return;
+    }
+    res.json({
+      "Villagers":rows
+    })
+  });
+});
+
+app.use(function(req, res){
+  console.error("404 Error");
+  res.status(404).json({"error": "404 - Sorry, the API cannot find that request"});
+});
+
+app.use(function(err, req, res, next) {
+  res.status(500).send("")
+});
+
+process.on('SIGINT', () => {
+  server.close(() => {
+    db.close((err) => {
+      if (err) {
+        return console.error(err.message);
+      }
+      console.log('Closed database connection');
+    });
+    console.log('Current process terminated');
+  });
+})
